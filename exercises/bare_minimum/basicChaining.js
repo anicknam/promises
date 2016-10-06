07095f2d3b427eb85df407e5c6c26ad6b80c0a06
anicknam <a.nicknam@gmail.com>
2016-10-06 15:56:18 -0700

    Kicked off Bare Minimum Requirements Exercises on Promises

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -10,11 +10,39 @@
 
 var fs = require('fs');
 var Promise = require('bluebird');
+var githubFunctions = require('./promisification');
 
+var readUsername = function(readFilePath) {
+  return new Promise(function(resolve, reject) {
+    fs.readFile(readFilePath, (err, content) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(content.toString().split('\n')[0]);
+        // console.log('first line of content IS', content.toString().split('\n')[0]+'END');
+      }
+    });
+  });
+};
 
+var writeResponse = function(content, writeFilePath) {
+  return new Promise(function(resolve, reject) {
+    console.log('content is ', content.login);
+    var fileToWriteTo = __dirname + '/../../test/files/file_to_write_to.txt';
+
+    fs.writeFile(fileToWriteTo, JSON.stringify(content), (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(JSON.stringify(content));
+      }
+    });
+  });
+};
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
-  // TODO
+  //readUsername(readFilePath).then(githubFunctions.getGitHubProfileAsync).then(console.log);
+  return readUsername(readFilePath).then(githubFunctions.getGitHubProfileAsync).then(writeResponse);
 };
 
 // Export these functions so we can test them
